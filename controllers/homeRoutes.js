ad71e3b4b7f981e72745b9547c0eb469133e3442
sploganathan <sakthiloganathan1127@gmail.com>
2023-02-28 12:32:33 -0800

    feat(routes): Created routes for category view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,4 +51,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+// this is going to render the category view when category button is pressed
+router.get("/category", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+
+    const categories = categoryData.map((category) =>
+      category.get({ plain: true })
+    );
+
+    res.render("categoryView", {
+      categories,
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
