1c63ad442ee03f72ad0fa4961bdecdf2a6d324be
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-03-03 18:12:35 -0800

    comment previous post command - without images

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,18 +15,21 @@ router.post("/", upload.single("file"), createPost);
 
 
 // for creation of new post (without images)
-router.post("/", async (req, res) => {
-  try {
-    const postData = await Post.create(req.body);
-    if (postData) {
-      res.status(200).json({ message: "Post created successfully!" });
-    } else {
-      res.status(404).json({ message: "insufficient data" });
-    }
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const postData = await Post.create(req.body);
+//     if (postData) {
+//       res.status(200).json({ message: "Post created successfully!" });
+//     } else {
+//       res.status(404).json({ message: "insufficient data" });
+//     }
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+
 // to get all the posts
 router.get("/", async (req, res) => {
   try {
