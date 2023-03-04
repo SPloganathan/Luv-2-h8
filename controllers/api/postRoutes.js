12f7052463b69e6386513aae93b6afa5d639cea3
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-03-03 16:49:40 -0800

    new file postController - function for creating post and send image to cloudinary; add this function to our postRoutes; add multer package for srorage uploaded images between front-end and back-end to package.json

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,20 @@
 // importing required files
 const router = require("express").Router();
 const { Post, User, Business, Category } = require("../../models");
-// for creation of new post
+
+//add imported files for image import/uploading to cloudinary
+const { createPost } = require("../../controllers/postController");
+const multer = require("multer");
+
+// Configure multer middleware to store uploaded images in the uploads folder on our server (these will be removed later)
+const upload = multer({ dest: "uploads/" });
+
+
+// for creation of new post (with images)
+router.post("/", upload.single("file"), createPost);
+
+
+// for creation of new post (without images)
 router.post("/", async (req, res) => {
   try {
     const postData = await Post.create(req.body);
